471bf39ef1ff0e1525fed0b643f99f2ad9ba86f5
Guilherme <guilhermegalli5@gmail.com>
2020-07-04 16:16:21 -0300

    refactor App.js to use new components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
-import api from './api';
-import PokecardsList from './components/PokecardsList';
+import api from './utils/api';
+import Pokecard from './components/Pokecard';
 import Cart from './components/Cart';
+import randomInt from './utils/randomInt';
+import Modal from './components/Modal';
+import Button from './components/Button';
 
 function App() {
 	const [pokemons, setPokemons] = useState([]);
@@ -19,11 +22,14 @@ function App() {
 		const all = await Promise.all(results.map(item => api.get(item.url)));
 
 		const data = all.map(response => {
-			let data = response.data;
+			const { id, name, abilities } = response.data;
+
 			return {
-				id: data.id,
-				name: data.name,
-				image: `https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`
+				id,
+				name,
+				image: `https://pokeres.bastionbot.org/images/pokemon/${id}.png`,
+				price: randomInt(100, 10000),
+				abilities: abilities.map(item => item.ability.name)
 			}
 		});
 
@@ -32,18 +38,64 @@ function App() {
 
 	useEffect(() => {
 		getAllPokemons()
-	}, [])
+	}, []);
 
+	const [cartItems, setCartItems] = useState([]);
+	const [totalPrice, setTotalPrice] = useState(0);
+
+	const addToCart = (item) => {
+		if (cartItems.some(data => data.id === item.id)) {
+			return alert(`O ${item.name} já está no carrinho.`);
+		}
+		setCartItems([...cartItems, item]);
+	}
+
+	const calculateTotal = () => {
+		const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+		const total = cartItems.length > 0 ? cartItems.reduce(reducer, 0) : 0;
+		setTotalPrice(total)
+	}
+
+	useEffect(() => {
+		calculateTotal()
+	}, [cartItems])
+
+	const [finishCartModal, setFinishCartModal] = useState(false);
+
+	const finishModalHandler = () => setFinishCartModal(!finishCartModal);
+	const onCloseFinishModal = () => {
+		setCartItems([]);
+		setFinishCartModal(false);
+	}
 
 	return (
 		<main>
 			<section className="shopping">
 				<div className="shopping-list">
 					<h1>Pokecommerce</h1>
-					<PokecardsList pokemons={pokemons} />
+
+					<div className="pokecards-container">
+						<div className="pokecards-list">
+							{pokemons.map(pokemon => (
+								<Pokecard
+									key={pokemon.id}
+									pokemon={pokemon}
+									addToCart={() => addToCart(pokemon)}
+								/>
+							))}
+						</div>
+					</div>
 				</div>
-				<Cart />
+
+				<Cart items={cartItems} total={totalPrice} finishCart={finishModalHandler}/>
 			</section>
+			{finishCartModal ? 
+				<Modal onClose={onCloseFinishModal}>
+					<h3 className="finishCart-text">Compra realizada com sucesso!</h3>
+					<Button onClick={onCloseFinishModal} className="finishCart-btn">OK</Button>
+				</Modal>
+				: null
+			}
 		</main>
 	);
 }
